11aa7bdaff0c99694254ea926496113f2b906958
Aldrin Azucena <43855657+aazucena@users.noreply.github.com>
2022-12-01 01:18:45 -0700

    added more navigation

--- src/js/routes.js
+++ src/js/routes.js
@@ -1,11 +1,21 @@
 
 import HomePage from '../pages/home.f7'
 import NotFoundPage from '../pages/404.f7'
+import QRGeneratePage from '../pages/qr/generate.f7'
+import QRScanPage from '../pages/qr/scan.f7'
 
 var routes = [
   {
     path: '/',
     component: HomePage,
+  },
+  {
+    path: '/qr/generate/',
+    component: QRGeneratePage,
+  },
+  {
+    path: '/qr/scan/',
+    component: QRScanPage,
   },
   {
     path: '(.*)',
