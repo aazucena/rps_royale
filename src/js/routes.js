9bd9954d929a99c965fa88e2d9fb02dcbcd654bb
Aldrin Azucena <43855657+aazucena@users.noreply.github.com>
2022-12-02 01:09:30 -0700

    add directus and the signin/login pages

--- src/js/routes.js
+++ src/js/routes.js
@@ -3,6 +3,8 @@ import HomePage from '../pages/home.f7'
 import NotFoundPage from '../pages/404.f7'
 import QRGeneratePage from '../pages/qr/generate.f7'
 import QRScanPage from '../pages/qr/scan.f7'
+import SignInPage from '../pages/signin.f7'
+import LogInPage from '../pages/login.f7'
 
 var routes = [
   {
@@ -16,6 +18,14 @@ var routes = [
   {
     path: '/qr/scan/',
     component: QRScanPage,
+  },
+  {
+    path: '/signin/',
+    component: SignInPage,
+  },
+  {
+    path: '/login/',
+    component: LogInPage,
   },
   {
     path: '(.*)',
---
+++ src/js/services/directus.js
@@ -0,0 +1,3 @@
+import { Directus } from "@directus/sdk"
+
+export default new Directus("https://jvzl9v2g.directus.app")
